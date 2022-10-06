ff43c26e71d8c1481f7a69861936b5b2d0a383c9
Lenovo <Lenovo@ELYOR-05>
2022-10-06 23:38:04 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y, z;
+	cin >> x >> y >> z;
+	if (x + y > z && x + z > y && y + z > x)
+		cout << "Valid";
+	else
+		cout << "Invalid";
+	return 0;
+}
